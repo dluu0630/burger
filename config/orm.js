cef7fade1e7c48a734760cc3a55329183efa530d
dluu0630 <davidluu0630@gmail.com>
2017-09-25 18:40:21 -0700

    push

--- config/orm.js
+++ config/orm.js
@@ -9,19 +9,19 @@ if (process.env.JAWSDB_URL) {
 	connection = mysql.createConnection({
 		host: 'localhost',
 		user: 'root',
-		password: '',
+		password: 'Semper86',
 		database: 'burgers_db'
 	});
 };
 
 
-//var connection = mysql.createConnection({
-// 	host: "localhost",
-// 	port: 3306,
-//	user: "root",
-// 	password: "",
-// 	database: "burgers_db" 
-// });
+var connection = mysql.createConnection({
+ 	host: "localhost",
+ 	port: 3306,
+	user: "root",
+ 	password: "Semper86",
+ 	database: "burgers_db" 
+ });
 
 
 connection.connect(function(err) {
