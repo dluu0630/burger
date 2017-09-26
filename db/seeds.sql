fd8d835b976f90da7d2af057c35db560b8ed17b6
dluu0630 <davidluu0630@gmail.com>
2017-09-25 19:55:53 -0700

    push

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,5 +3,5 @@ USE `burgers_db`;
 
 -- insert 3 burgers
 INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Double Double', false, CURRENT_TIMESTAMP);
-INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Roast Beef', false, CURRENT_TIMESTAMP);
-INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Quarter Pounder', false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Flying Dutchman', false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Animal Style', false, CURRENT_TIMESTAMP);
